42043b17fbdb592227a367899adb0edc3fe909c3
eungi-hong <189397150+eungi-hong@users.noreply.github.com>
2026-01-26 16:54:19 +0800

    Echo

--- src/main/java/Bee.java
+++ src/main/java/Bee.java
@@ -1,7 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Bee {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello! I'm Bee");
         System.out.println("What can I do for you?");
+        String input;
+
+        while (!((input = sc.nextLine().toLowerCase()).equals("bye"))) {
+            System.out.println(input);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
